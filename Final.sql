8eb7b57bd0f098078f708050541224f2758e57f9
mdmrs <santokun0@gmail.com>
2023-03-26 16:55:37 +0600

    added values into course,instructor,student table

--- Final.sql
+++ Final.sql
@@ -60,3 +60,24 @@ ALTER TABLE `course` DROP PRIMARY KEY,
 
 ALTER TABLE student ADD COLUMN email varchar(30) UNIQUE;
 ALTER TABLE instructor ADD COLUMN email varchar(30) UNIQUE;
+
+
+Insert into course values 
+("MAT116","Pre-Caluculas"),
+("MAT120","Calculas-I"),
+("MAT130","Calculas-II"),
+("MAT250","Calculas-III")
+
+Insert into instructor values 
+(451324134,"Isabella","78429561"),
+(461116413,"Liam","39286745"),
+(474532132,"Emilia","14872693"),
+(491315644,"Ethan","97651203"),
+(502354654,"Sophia","63092418")
+
+Insert into student values
+(101555654,"Harper","98342765"),
+(102312324,"Miles","57461023"),
+(103556456,"Jocelyn","12873594"),
+(104235641,"Xavier","73986214"),
+(106231546,"Savannah","46219873")
